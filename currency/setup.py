ca138e7ce092139b1b678623969b55c2951a717c
miladmirza75 <miladmirza75@gmail.com>
2024-07-28 21:11:02 +0330

    Update setup.py

--- currency/setup.py
+++ currency/setup.py
@@ -1,12 +1,25 @@
-from setuptools import setup
+# setup.py
+from setuptools import setup, find_packages
 
 setup(
-    name='currency',
-    version='0.1',
-    description='A package for formatting currency values',
-    author='Milad Mirza',
-    author_email='Miladmirza75@gmail.com',
-    url='https://github.com/miladmirza75/currency',
-    packages=['currency'],
-    install_requires=[],
+    name='currencycomma2',
+    version='0.2',  # Update the version number
+    description='A package to format numbers with commas',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/currencycomma2',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    python_requires='>=3.7',
 )
